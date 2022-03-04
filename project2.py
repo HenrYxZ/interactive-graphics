c483eac09c1d495f773e767ac5f9025a0ace9d01
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-03-04 18:36:56 -0300

    feat: project2 working

--- project2.py
+++ project2.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from pyrr import Matrix44
 import struct
 
@@ -6,13 +7,27 @@ import moderngl
 import moderngl_window as mglw
 
 
-class LoadingOBJ(mglw.WindowConfig):
+# Local modules
+from constants import *
+import utils
+
+
+class Window(mglw.WindowConfig):
     gl_version = (3, 3)
     title = "ModernGL Example"
     window_size = (1280, 720)
     aspect_ratio = 16 / 9
     resizable = True
     resource_dir = os.path.normpath(os.path.join(__file__, '../data'))
+    cam_target = np.array([0, 0, 0])
+    currently_pressed = 0
+    sensitivity = 0.1
+    angles_delta = 10
+    zoom_speed = 0.1
+    theta = 90
+    phi = 90
+    zoom = 3
+    up = (0, 0, 1)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,9 +39,10 @@ class LoadingOBJ(mglw.WindowConfig):
                 #version 330
                 in vec3 in_position;
                 uniform float scale;
+                uniform mat4 Mvp;
                 void main() {
                     vec3 pos = scale * in_position;
-                    gl_Position = vec4(pos, 1.0);
+                    gl_Position = Mvp * vec4(pos, 1.0);
                 }
             ''',
             fragment_shader='''
@@ -37,24 +53,50 @@ class LoadingOBJ(mglw.WindowConfig):
                 }
             ''',
         )
-        # self.mvp = self.prog['Mvp']
+        self.mvp = self.prog['Mvp']
         self.scale = self.prog['scale']
         # Create a vao from the first root node (attribs are auto mapped)
         self.vao = self.obj.root_nodes[0].mesh.vao.instance(self.prog)
+
+    def mouse_press_event(self, x, y, button):
+        if 0 <= x <= self.window_size[0] and 0 <= y <= self.window_size[1]:
+            self.currently_pressed = button
+
+    def mouse_release_event(self, x, y, _):
+        if 0 <= x <= self.window_size[0] and 0 <= y <= self.window_size[1]:
+            self.currently_pressed = 0
+
+    def mouse_drag_event(self, _, __, dx, dy):
+        if self.currently_pressed == LEFT_BUTTON:
+            # Adjust camera angle
+            delta_theta = dx * self.sensitivity * self.angles_delta
+            self.theta += delta_theta
+            delta_phi = dy * self.sensitivity * self.angles_delta
+            self.phi += delta_phi
+        elif self.currently_pressed == RIGHT_BUTTON:
+            # Adjust zoom
+            self.zoom += self.zoom_speed * dy
 
     def render(self, time, frame_time):
         self.ctx.clear(0.0, 0.0, 0.0)
         self.ctx.enable(moderngl.DEPTH_TEST)
 
+        fov = 45.0
+        far = 1000.0
+        near = 0.1
         proj = Matrix44.perspective_projection(
-            45.0, self.aspect_ratio, 0.1, 1000.0
+            fov, self.aspect_ratio, near, far
         )
-        eye = (0.0, 0.0, -3.0)
+        theta_rads = np.radians(self.theta)
+        phi_rads = np.radians(self.phi)
+        eye_x = self.zoom * np.cos(theta_rads) * np.sin(phi_rads)
+        eye_y = self.zoom * np.sin(theta_rads) * np.sin(phi_rads)
+        eye_z = self.zoom * np.cos(phi_rads)
+        eye = (eye_x, eye_y, eye_z)
         target = (0.0, 0.0, 0.0)
-        up = (0.0, 1.0, 0.0)
-        lookat = Matrix44.look_at(eye, target, up)
+        lookat = Matrix44.look_at(eye, target, self.up)
 
-        # self.mvp.write((proj * lookat).astype('f4'))
+        self.mvp.write((proj * lookat).astype('f4'))
         self.scale.write(struct.pack('f', 0.05))
         self.vao.render(moderngl.POINTS)
 
@@ -64,4 +106,4 @@ class LoadingOBJ(mglw.WindowConfig):
 
 
 if __name__ == '__main__':
-    LoadingOBJ.run()
+    Window.run()
