1fca9b65b558edfdcf49118994dece0f6ae54d31
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-10-25 20:36:16 -0300

    wip: project4 specular map not working

--- project4.py
+++ project4.py
@@ -1,3 +1,6 @@
+import moderngl
+
+
 from constants import SPECULAR_ALPHA
 from orbit_camera import OrbitCameraWindow
 
@@ -7,6 +10,8 @@ class Window(OrbitCameraWindow):
         scene_path = 'data/teapot.obj'
         program_path = 'shaders/textured.glsl'
         super(Window, self).__init__(scene_path, program_path, **kwargs)
+        self.specular_tex = self.load_texture_2d('data/brick-specular.png')
+        self.specular_tex.build_mipmaps()
         # Activate texture
         self.obj.root_nodes[0].mesh.material.mat_texture.texture.use()
         self.set_light_col()
@@ -14,6 +19,7 @@ class Window(OrbitCameraWindow):
         self.set_specular_alpha()
         self.uses_eye = True
         self.uses_light = True
+        self.prog['texture1'].value = 1
 
     # Set Shader Attributes
     def set_light_col(self):
@@ -24,6 +30,16 @@ class Window(OrbitCameraWindow):
 
     def set_specular_alpha(self):
         self.prog['alpha'] = SPECULAR_ALPHA
+
+    def render(self, time, frame_time):
+        self.ctx.clear(0.0, 0.0, 0.0)
+        self.ctx.enable(moderngl.DEPTH_TEST)
+
+        self.update_mvp()
+        if self.uses_light:
+            self.update_light()
+        self.specular_tex.use(location=1)
+        self.vao.render(moderngl.TRIANGLES)
 
 
 if __name__ == '__main__':
--- shaders/textured.glsl
+++ shaders/textured.glsl
@@ -27,6 +27,7 @@ uniform vec3 light_col;
 uniform vec3 light_pos;
 uniform vec3 ambient_col;
 uniform sampler2D texture0;
+uniform sampler2D texture1;
 uniform vec3 eye;
 uniform float alpha;
 
@@ -47,7 +48,8 @@ void main() {
     vec3 ambient = diff_col * ambient_col;
     vec3 view = normalize(eye - v_vert);
     vec3 h = normalize(l + view);
-    float specular = clamp(dot(h, v_norm), 0.0, 1.0);
+    // float specular = clamp(dot(h, v_norm), 0.0, 1.0);
+    float specular = texture(texture1, v_tex).r;
     vec3 color = (
         light_col * (Kd * diffuse + Ks * pow(specular, alpha)) +
         ambient_col * Ka * ambient
